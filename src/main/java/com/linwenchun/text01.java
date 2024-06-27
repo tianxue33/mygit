c0952519c35b00e47226d424e33a83446d692bb5
linwenchun <674742876@qq.com>
2024-06-27 21:52:43 +0800

    合并提交

--- src/main/java/com/linwenchun/text01.java
+++ src/main/java/com/linwenchun/text01.java
@@ -1,12 +1,9 @@
 package com.linwenchun;
 
 public class text01 {
-<<<<<<< HEAD
+
     public static void main(String[] args) {
-        System.out.println("dv2text01");
-=======
-    public static void main(String[] args){
-    System.out.println("dv合并text01");
->>>>>>> origin/dv
+        System.out.println("dv合并text01");
+
     }
 }
